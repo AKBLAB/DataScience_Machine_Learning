7283744aba3ccda4b8e22b5d4c5303c56c295d81
Ashwani Kumar <ashgoldy@live.com>
2019-10-08 07:31:12 +0530

    Exercise on knn cross validation

--- ML_Knn.R
+++ ML_Knn.R
@@ -99,3 +99,80 @@ train_set_x <- x[train_index,]
 test_set_x <- x[-train_index,]
 
 ####### ######## ######## ######### ######### #########
+
+
+
+## Cross Validation ##
+
+## Because x and y are completely independent, one should not be able to predict
+## y using x with accuracy greater than 0.5. Confirm this by running 
+## cross-validation using logistic regression to fit the model. Because we have 
+## so many predictors, we selected a random sample x_subset. 
+## Use the subset when training the model.
+
+set.seed(1996)
+
+n <- 1000
+p <- 10000
+x <- matrix(rnorm(n*p), n, p)
+colnames(x) <- paste("x", 1:ncol(x), sep = "_")
+y <- rbinom(n, 1, 0.5) %>% factor()
+
+x_subset <- x[ ,sample(p, 100)]
+
+## Which code correctly performs this cross-validation?
+
+fit <- train(x_subset, y, method = "glm")
+fit$results
+
+## BiocManager::install("genefilter")
+## library(genefilter)
+## tt <- colttests(x, y) 
+##
+## Which of the following lines of code correctly creates a vector of the 
+## p-values called pvals?
+
+pvals <- tt$p.value
+
+
+
+## Create an index ind with the column numbers of the predictors that were 
+## "statistically significantly" associated with y. Use a p-value cutoff of 
+## 0.01 to define "statistically significantly."
+
+## How many predictors survive this cutoff?
+
+ind <- which(pvals <= 0.01)
+length(ind)
+
+# Another correct method tried 
+tt <- colttests(x, y)
+pvals <- tt$p.value
+sum(pvals<0.01)
+
+
+
+## re-run the cross-validation after redefinining x_subset to be the subset of x
+## defined by the columns showing "statistically significant" association with y.
+
+## What is the accuracy now?
+
+x_subset <- x[,ind]
+fit <- train(x_subset, y, method = "glm")
+fit$results
+
+
+
+## Use the train function with kNN to select the best k for predicting tissue 
+## from gene expression on the tissue_gene_expression dataset from dslabs. 
+## Try k = seq(1,7,2) for tuning parameters. 
+# For this question, do not split the data into test and train sets 
+## (understand this can lead to overfitting, but ignore this for now).
+
+## What value of k results in the highest accuracy?
+
+data("tissue_gene_expression")
+fit <- with(tissue_gene_expression, train(x, y, method = "knn", tuneGrid = data.frame( k = seq(1, 7, 2))))
+ggplot(fit)
+fit$results
+
